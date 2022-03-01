146bda6192dc2390b661412ed6cec2ed9d969e07
Leon Niederpruem <leon.niederpruem@ivx.com>
2022-03-01 11:37:39 +0100

    Add check_number method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
-class FizzBuzz
+class FizzBuzzGame
+  def check_number(number)
+    return 'fizzbuzz' if (number % 15).zero?
+
+    return 'buzz' if (number % 5).zero?
+
+    return 'fizz' if (number % 3).zero?
+
+    number
+  end
 end
