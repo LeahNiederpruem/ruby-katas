86e6302a8d78bba2cfbdec9460a2b2c060441e62
Leon Niederpruem <leon.niederpruem@ivx.com>
2022-03-02 14:13:08 +0100

    Add method check_portfolio

--- lib/portfolio.rb
+++ lib/portfolio.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class StockPortfolio
+  def check_portfolio(portfolio)
+    portfolio
+  end
 end
--- spec/portfolio_spec.rb
+++ spec/portfolio_spec.rb
@@ -3,4 +3,17 @@
 require 'portfolio'
 
 RSpec.describe StockPortfolio do
+  subject(:portfolio) { described_class.new }
+
+  empty_portfolio = {}
+
+  filled_portfolio = { IBM: 12, AAPL: 1 }
+
+  it 'checks if portfolio is empty' do
+    expect(portfolio.check_portfolio(empty_portfolio)).to eq(empty_portfolio)
+  end
+
+  it 'checks if portfolio is NOT empty' do
+    expect(portfolio.check_portfolio(filled_portfolio)).to eq(filled_portfolio)
+  end
 end
