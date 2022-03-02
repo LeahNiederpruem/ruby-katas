0ed5b3ba568f1603fff9c05ff74a524526cb7334
Leon Niederpruem <leon.niederpruem@ivx.com>
2022-03-02 13:22:20 +0100

    Add boilerplate

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,4 @@
 # frozen_string_literal: true
 
-class FizzBuzzGame
-  def check_number(number)
-    return 'fizzbuzz' if (number % 15).zero?
-
-    return 'buzz' if (number % 5).zero?
-
-    return 'fizz' if (number % 3).zero?
-
-    number
-  end
+class BlahClass
 end
