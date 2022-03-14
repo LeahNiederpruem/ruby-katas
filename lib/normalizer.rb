46ccc80cd13799ccf4406154569afb856389c777
Leon Niederpruem <leon.niederpruem@ivx.com>
2022-03-14 11:38:01 +0100

    Refactor code

--- lib/normalizer.rb
+++ lib/normalizer.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 class NameNormalizer
-  def normalize(name, surname, middlename = '')
-    name = name.strip
-    surname = surname.strip
-    middlename = middlename.strip
+  def initialize(name = '', surname = '', middlename = '')
+    @name = name.strip
+    @surname = surname.strip
+    @middlename = middlename.strip
+  end
 
+  def normalize
     return name if surname.empty?
 
     return "#{surname}, #{name}" if middlename.empty?
 
     "#{surname}, #{name} #{middlename[0]}."
   end
+
+  private
+
+  attr_reader :name, :surname, :middlename
 end
--- spec/normalizer_spec.rb
+++ spec/normalizer_spec.rb
@@ -6,30 +6,30 @@ RSpec.describe NameNormalizer do
   it 'returns empty string given an empty string' do
     normalizer = NameNormalizer.new
 
-    expect(normalizer.normalize('', '')).to eq('')
+    expect(normalizer.normalize).to eq('')
   end
 
   it 'returns a mononym' do
-    normalizer = NameNormalizer.new
+    normalizer = NameNormalizer.new('Linus')
 
-    expect(normalizer.normalize('Linus', '')).to eq('Linus')
+    expect(normalizer.normalize).to eq('Linus')
   end
 
   it 'swaps the name and surname' do
-    normalizer = NameNormalizer.new
+    normalizer = NameNormalizer.new('Linus', 'Torvalds')
 
-    expect(normalizer.normalize('Linus', 'Torvalds')).to eq('Torvalds, Linus')
+    expect(normalizer.normalize).to eq('Torvalds, Linus')
   end
 
   it 'trims leading and trailing whitespace' do
-    normalizer = NameNormalizer.new
+    normalizer = NameNormalizer.new('   Linus   ', 'Torvalds    ')
 
-    expect(normalizer.normalize('   Linus   ', 'Torvalds  ')).to eq('Torvalds, Linus')
+    expect(normalizer.normalize).to eq('Torvalds, Linus')
   end
 
   it 'initializes the middle name' do
-    normalizer = NameNormalizer.new
+    normalizer = NameNormalizer.new('Linus', 'Torvalds', 'Benedict')
 
-    expect(normalizer.normalize('Linus', 'Torvalds', 'Benedict')).to eq('Torvalds, Linus B.')
+    expect(normalizer.normalize).to eq('Torvalds, Linus B.')
   end
 end
