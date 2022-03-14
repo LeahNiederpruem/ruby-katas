657ce8b5113dfe222f1bb567c3f33f8acb5f4fec
Leon Niederpruem <leon.niederpruem@ivx.com>
2022-03-14 15:47:47 +0100

    Add method to return a word

--- lib/dictionary/dictionary.rb
+++ lib/dictionary/dictionary.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Dictionary
+  def word(word)
+    word
+  end
 end
--- spec/dictionary/dictionary_spec.rb
+++ spec/dictionary/dictionary_spec.rb
@@ -3,4 +3,9 @@
 require './lib/dictionary/dictionary'
 
 RSpec.describe Dictionary do
+  it 'returns a word' do
+    dictionary = Dictionary.new
+
+    expect(dictionary.word('Table')).to eq('Table')
+  end
 end
