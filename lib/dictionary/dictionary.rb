d504b93306fcc2b911e729a8f1133f068dededff
Leon Niederpruem <leon.niederpruem@ivx.com>
2022-03-14 15:38:27 +0100

    Initial commit

--- lib/dictionary/dictionary.rb
+++ lib/dictionary/dictionary.rb
@@ -1,45 +1,4 @@
 # frozen_string_literal: true
 
 class Dictionary
-  def initialize
-    @content = {}
-  end
-
-  def definition(word)
-    content[word]&.flatten
-  end
-
-  def add(word, definition)
-    raise 'Cannot store empty string' if word.empty?
-
-    content[word] = definition
-  end
-
-  def delete_word(word)
-    content.delete(word)
-  end
-
-  def delete_definition(word, definition)
-    content[word].delete(definition)
-  end
-
-  def append(word, definition)
-    content[word].append(definition)
-  end
-
-  def total_words
-    content.keys.size
-  end
-
-  def total_definitions
-    content.values.flatten.size
-  end
-
-  def empty?
-    content.empty?
-  end
-
-  private
-
-  attr_reader :content
 end
